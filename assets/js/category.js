de915fe02751d22e9a6ab021f19433f4b985e016
Sangwon Lee <gumgood@naver.com>
2022-12-25 13:46:23 +0900

    Refactor: categories, footer

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,7 +1,7 @@
-const i_toggles = document.querySelectorAll("i.toggle").forEach((i) => {
+document.querySelectorAll("i.triangle-down").forEach((i) => {
     i.addEventListener("click", function () {
-        i.classList.toggle('rotate');
+        i.toggleAttribute('rotate');
         for (var li = i.parentNode; (li = li.nextElementSibling) && li.classList.contains('subcategory');)
-            li.classList.toggle('hidden');
+            li.toggleAttribute('hidden');
     });
 });
