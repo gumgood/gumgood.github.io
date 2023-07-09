aa1ea9baeac2ba9ef9916d1c1d598e6b7384121a
Sangwon Lee <gumgood@naver.com>
2023-07-09 13:00:53 +0900

    Refactor: category inline script

--- assets/js/category.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("i.triangle-down").forEach((i) => {
-    i.addEventListener("click", function () {
-        i.toggleAttribute('rotate');
-        for (var li = i.parentNode; (li = li.nextElementSibling) && li.classList.contains('subcategory');)
-            li.toggleAttribute('hidden');
-    });
-});
