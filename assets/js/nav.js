5b264e21b0f2c42bc27f66bfdae75ca516199a7f
Sangwon Lee <gumgood@naver.com>
2023-07-09 10:14:06 +0900

    Design: fix nav media query

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,13 +1,19 @@
-three_bars = document.querySelector("i.three-bars").parentElement;
-x = document.querySelector("i.x").parentElement;
-nav = document.querySelector("nav");
-
-console.log(three_bars);
-console.log(x);
+const three_bars = document.querySelector("i.three-bars").parentElement;
+const x = document.querySelector("i.x").parentElement;
+const nav = document.querySelector("nav");
 
 three_bars.addEventListener("click", function () {
     nav.style.left = "0";
 });
 x.addEventListener("click", function () {
     nav.style.left = "-260px";
+});
+
+const w_min = window.matchMedia("screen and (min-width: 850px)");
+w_min.addEventListener("change", function (e) {
+    if (e.matches) {
+        nav.style.left = "0";
+    } else {
+        nav.style.left = "-260px";
+    }
 });
